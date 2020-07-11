14e6ca27d2ac27da74231c23d3ed5d0df11f29da
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2020-07-11 18:17:08 +0530

    validations

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ const CSVDataEl = document.getElementById('CSVTextArea');
 const JSONDataEl = document.getElementById('JSONTextArea');
 const covertToJSONBtn = document.getElementById('CSVtoJSONBtn');
 const covertToCSVBtn = document.getElementById('JSONtoCSVBtn');
+const CSVErrorEl = document.querySelector('.input__error--csv');
+const JSONErrorEl = document.querySelector('.input__error--json');
 
 let CSVData;
 let JSONData;
@@ -16,19 +18,56 @@ function validateCSV(inputCSVText) {
   //length check
   let length = CSVDataArray[0].length;
   const isValidArray = CSVDataArray.filter((row) => row.length !== length);
+
   return {
     isValidCSV: isValidArray.length === 0 ? true : false,
     CSVDataArray,
+    errorMsg:
+      isValidArray.length === 0 ? '' : 'There is an error in the CSV Format',
   };
 }
 
+function JSONRowCheck(JSONDataObj) {
+  const keys = Object.keys(JSONDataObj[0]);
+  let errorMsg = '';
+  JSONDataObj.forEach((row) => {
+    Object.values(row).forEach((el) => {
+      if (typeof el == 'object') {
+        errorMsg =
+          'Complex datatypes cannot be converted to CSV Format. Please use a flat structure';
+      } else if (el.includes(',')) {
+        errorMsg = 'Please remove any commas (,) in the JSON data';
+      }
+    });
+    if (
+      // checks if the sets of keys (array) are equal are equal
+      !(
+        keys.length === Object.keys(row).length &&
+        keys.every((value, index) => value === Object.keys(row)[index])
+      )
+    ) {
+      errorMsg =
+        'The JSON Keys do not seem to be consistent. Kindly use a consistent format';
+    }
+  });
+  return errorMsg;
+}
+
 function validateJSON(inputJSONText) {
-  let JSONDataObj = JSON.parse(inputJSONText);
-  isValidArray = [];
-  return {
-    isValidJSON: isValidArray.length === 0 ? true : false,
-    JSONDataObj,
-  };
+  let errorMsg = '';
+  let JSONDataObj;
+  try {
+    JSONDataObj = JSON.parse(inputJSONText);
+    errorMsg = JSONRowCheck(JSONDataObj);
+  } catch (err) {
+    errorMsg = err.message;
+  } finally {
+    return {
+      isValidJSON: errorMsg.length === 0 ? true : false,
+      JSONDataObj,
+      errorMsg,
+    };
+  }
 }
 
 function CSVtoJSON(CSVDataArray) {
@@ -48,43 +87,50 @@ function updateDOMForJSON(jsonData) {
   JSONDataEl.value = JSON.stringify(jsonData);
   toggleJSONToCSVBtn();
 }
-function updateDOMForCSV(CSVData) {
-  console.log(CSVData);
 
+function updateDOMForCSV(CSVData) {
   CSVDataEl.value = CSVData;
   toggleCSVToJSONBtn();
 }
-function printCSVError() {
-  console.log('Error in CSV Format');
+
+function clearErrorMsg() {
+  CSVErrorEl.textContent = '';
+  JSONErrorEl.textContent = '';
+}
+
+function printCSVError(errorMsg) {
+  errorMsg = errorMsg || 'Error in CSV';
+  CSVErrorEl.textContent = errorMsg;
   return;
 }
-function printJSONError() {
-  console.log('Error in JSON');
+function printJSONError(errorMsg) {
+  errorMsg = errorMsg || 'Error in JSON';
+  JSONErrorEl.textContent = errorMsg;
   return;
 }
 
 function JSONtoCSV(JSONObj) {
   let CSVDataArray = [];
   CSVDataArray.push(Object.keys(JSONObj[0]).join(','));
-
   JSONObj.forEach((el) => {
     CSVDataArray.push(Object.values(el).join(','));
   });
-
   updateDOMForCSV(CSVDataArray.join('\r\n'));
 }
 
 function convertToJSONBtnClick() {
+  clearErrorMsg();
   let CSVData = CSVDataEl.value;
-  // let? isValidCSV,CSVDataArray;
-  let { isValidCSV, CSVDataArray } = validateCSV(CSVData);
-  isValidCSV ? CSVtoJSON(CSVDataArray) : printCSVError();
+  let { isValidCSV, CSVDataArray, errorMsg } = validateCSV(CSVData);
+
+  isValidCSV ? CSVtoJSON(CSVDataArray) : printCSVError(errorMsg);
 }
 
 function convertToCSVBtnClick() {
+  clearErrorMsg();
   let JSONData = JSONDataEl.value;
-  let { isValidJSON, JSONDataObj } = validateJSON(JSONData);
-  JSONtoCSV(JSONDataObj);
+  let { isValidJSON, JSONDataObj, errorMsg } = validateJSON(JSONData);
+  isValidJSON ? JSONtoCSV(JSONDataObj) : printJSONError(errorMsg);
 }
 
 function toggleCSVToJSONBtn() {
@@ -112,33 +158,31 @@ CSVDataEl.innerHTML = `PHONE,COUNTRY_CODE
 (601) 291-7292 x737,0ad705f8-c0cd-49ab-a3d1-5a35a594218d 
 (714) 845-8952 x6918,f9e64435-feab-497e-b992-e6e94e272863`;
 
-/* 
-[
+JSONDataEl.innerHTML = `[
     {
-        "TestA": [
-            "michellemathews@gmail.com",
-            "hwalker@gmail.com",
-            "justin32@deleon.org",
-            "xmorales@hotmail.com"
-        ],
-        "TestB": {
-            "try1": "087",
-            "try2": "ii8Y"
-        },
-        "TestC": "PSC 1031, Box 0939\nAPO AE 48108"
+        "A": "Mrs. ,Diana Clark",
+        "B": "71-0248690",
+        "C": "Smith",
+        "D": "Troy Benson",
+        "E": "Benjamin",
+        "F": "ex59W7qM%h",
+        "G": "silver",
+        "H": "3568305293725728",
+        "I": "line",
+        "J": "05",
+        "K": "/performance/parent.png"
     },
     {
-        "TestA": [
-            "willie84@hotmail.com",
-            "courtneysimpson@hotmail.com",
-            "zperez@rodriguez.com",
-            "anthonypearson@gmail.com",
-            "mlopez@yahoo.com",
-            "lisa04@coffey-myers.com"
-        ],
-        "TestB": {
-            "try1": "31",
-            "try2": "V2i"
-        },
-        "TestC": "Unit 6958 Box 5139\nDPO AE 89806"
-    }] */
+        "A": "Ricky Hall",
+        "B": "26-7773288",
+        "C": "Peterson",
+        "D": "Calvin Vega",
+        "E": "Nicholas",
+        "F": "Z+7#Ev#&vb",
+        "G": "green",
+        "H": "503889756782",
+        "I": "where",
+        "J": "07",
+        "K": "/free/address.wav"
+    }
+]`;
